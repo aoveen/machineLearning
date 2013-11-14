01a4464f054f42986c78ef912fc2ce9f67d15d6a
Hector Dearman <hector.dearman@gmail.com>
2013-11-14 11:46:55 +0000

    initial version of optimise layers for single layer

--- src/neural_networks/create_nn.m
+++ src/neural_networks/create_nn.m
@@ -1,8 +1,7 @@
 function [ net ] = create_nn( x, y, train_validation_boundary, params )
 %CREATE_SIX_OUTPUT_NN Create a neural network with 6 outputs.
     [x, y] = ANNdata(x, y);
-    hidden_layers = 2;
-    net = feedforwardnet(hidden_layers);
+    net = feedforwardnet(params('hiddenSizes'));
     net = configure(net, x, y);
     net.divideFcn = 'divideind';
     net.divideParam.valInd = 1:train_validation_boundary;
@@ -10,22 +9,8 @@ function [ net ] = create_nn( x, y, train_validation_boundary, params )
     net.trainFcn = params('trainFcn');
     
     for key = keys(params),
-       if strcmp(key{1}, 'epochs')
-           net.trainParam.epochs = params(key{1});
-       elseif strcmp(key{1}, 'goal')
-           net.trainParam.goal = params(key{1});
-       elseif strcmp(key{1}, 'max_fail')
-           net.trainParam.max_fail = params(key{1});
-       elseif strcmp(key{1}, 'min_grad')
-           net.trainParam.min_grad = params(key{1});
-       elseif strcmp(key{1}, 'show')
-           net.trainParam.show = params(key{1});
-       elseif strcmp(key{1}, 'showCommandLine')
-           net.trainParam.showCommandLine = params(key{1});
-       elseif strcmp(key{1}, 'showWindow')
-           net.trainParam.showWindow = params(key{1});
-       elseif strcmp(key{1}, 'time')
-           net.trainParam.time = params(key{1});
+       if ~(strcmp(key{1}, 'trainFcn') || strcmp(key{1}, 'hiddenSizes'))
+           net.trainParam.(key{1}) = params(key{1});
        end
     end
     
