b8f2fe91ab57da4310c5cfed3295c7b5f364beb7
Simon Stuckemann <ss7311@doc.ic.ac.uk>
2013-11-15 12:17:22 +0000

    Modified optimisation script to import path.

--- src/neural_networks/optimise_parameters.m
+++ src/neural_networks/optimise_parameters.m
@@ -1,2 +1,7 @@
-load('../support/cleandata_students.mat')
+addpath('../../support')
+addpath('./')
+addpath('./optimisation_functions')
+load('../../support/cleandata_students.mat')
+display('loaded paths and datasets')
 train_a_single_net(x,y)
+exit
