03f0d3bf85816bfba9ae96d54a0d82498fd29ec7
Hector Dearman <hector.dearman@gmail.com>
2013-11-16 16:20:41 +0000

    fixed bug

--- src/neural_networks/optimisation_functions/optimise_layers.m
+++ src/neural_networks/optimisation_functions/optimise_layers.m
@@ -5,21 +5,21 @@ function optimise_layers( x, y, params )
     min_layers = 1;
     max_layers = 3;
     bucket_size = 10;
-    
+
     node_num_buckets = min_in_layer:bucket_size:max_in_layer;
     [p, q] = meshgrid(min_layers:max_layers, node_num_buckets);
     possible_pairs = [p(:) q(:)]
-    
+
     vector_three_fold_optimise(x, y, possible_pairs, params, 'hidden_layers');
-    
+
     hl = params('hidden_layers');
     layers = hl(1)
     best_bucket = hl(2)
-    
+
     possible_node_num = floor(best_bucket-bucket_size/2):1:ceil(best_bucket+bucket_size/2);
     [p, q] = meshgrid(layers, possible_node_num);
     possible_pairs = [p(:) q(:)]
-    
-    three_fold_optimise(x, y, possible_pairs, params, 'hidden_layers');
+
+    vector_three_fold_optimise(x, y, possible_pairs, params, 'hidden_layers');
 end
 
