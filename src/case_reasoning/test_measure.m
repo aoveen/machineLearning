dd13d3f4e543e362fbfc2e3129b1614b2e029ead
Simon Stückemann <simon.stuckemann11@imperial.ac.uk>
2013-11-26 17:51:23 +0000

    Refactor for one function.

---
+++ src/case_reasoning/test_measure.m
@@ -0,0 +1,24 @@
+function test_measure(x, y, measure, name)
+    folds = 10;
+    newx = x(1:length(x), :);
+    newy = y(1:length(y));
+
+    confuse = zeros(6);
+    display(sprintf('Checking results for measure "%s"\n', name));
+
+    for j=1:folds
+        [testX, trainingX] = select_fold(newx, j, folds);
+        [testY, trainingY] = select_fold(newy, j, folds);
+        n = length(trainingY);
+        for k=1:n
+            cases(k) = Case(xs2aus(trainingX(k, :)), trainingY(k));
+        end
+        cbr = nearest_k_cbr(cases, measure, 10, true);
+        p = testCBR(cbr, testX);
+        confuse = confuse + calc_confusion_matrix(testY, p);
+    end
+    [p, r, f, e] = stats(confuse);
+    display(sprintf('RESULT %s = %f\n', name, e));
+    save(strcat('results_nearest_k10_noisy_', name), 'confuse', 'p', 'r', 'f', 'e');
+    %fflush(stdout); % Octave only
+end
